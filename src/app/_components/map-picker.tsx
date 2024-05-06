9ae908aa9bcac784e755966e262c1efa7c6a96cf
Sergei Ustinov <sergey.ustinov@sensand.com>
2024-05-06 14:24:24 +1000

    fix ts issues

--- src/app/_components/map-picker.tsx
+++ src/app/_components/map-picker.tsx
@@ -1,30 +1,18 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { useState, forwardRef, type ForwardedRef, LegacyRef } from "react";
+"use client";
+
+import { useState, forwardRef } from "react";
 import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import ngeohash from "ngeohash";
-import { type LatLngLiteral } from "leaflet";
+import { type LatLngLiteral, type Map } from "leaflet";
 
 interface LocationPickerProps {
   onChange: (location: string) => void;
 }
 
-/**
- * LocationPicker component for selecting a location on a map.
- * @param onChange - Callback function triggered when a location is selected.
- * @param ref - Reference to the component.
- * @returns JSX.Element - The rendered LocationPicker component.
- */
-const LocationPicker = forwardRef(
-  ({ onChange }: LocationPickerProps, ref: ForwardedRef<any>) => {
+const LocationPicker = forwardRef<Map, LocationPickerProps>(
+  ({ onChange }, ref) => {
     const [position, setPosition] = useState<LatLngLiteral>();
-
-    /**
-     * MapEvents component for handling map events.
-     * @returns null
-     */
 
     const MapEvents = () => {
       const map = useMapEvents({
@@ -32,7 +20,7 @@ const LocationPicker = forwardRef(
           const { lat, lng } = e.latlng;
           setPosition(e.latlng);
           const geohash = ngeohash.encode(lat, lng);
-          onChange(geohash); // Fix: Update the type of the onChange function parameter to accept a string instead of a Location.
+          onChange(geohash);
         },
       });
       return null;
